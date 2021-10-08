d8dea11f3ae662ddf36086d68cfa12b0af361b90
Svetlana Stoycheva <s.stoycheva@gmail.com>
2021-10-08 18:13:05 +0300

    contact page completed

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+import { SiFrontendmentor } from 'react-icons/si';
 
 const ContactPage = () => {
-  return <div></div>;
+  return (
+    <section className='contact-container'>
+      <hr className='long-line' />
+      <h3 className='section-title'>get in touch</h3>
+      <div className='contact-info'>
+        <p>
+          If you are looking to get ahold of me, you can send me an email at{' '}
+          <span className='contacts-email'>s.stoycheva@gmail.com</span>
+        </p>
+        <hr className='long-line' />
+        <div className='contact-social-link-container'>
+          <li className=' contact-social-link'>
+            <a
+              href='https://github.com/SvetlanaStoycheva'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              <FaGithub />
+            </a>
+          </li>
+          <li className='contact-social-link'>
+            <a
+              href='https://www.frontendmentor.io/profile/SvetlanaStoycheva/solutions'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              <SiFrontendmentor />
+            </a>
+          </li>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ContactPage;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ import img03 from './gallery-images/img-03.png';
 import img04 from './gallery-images/img-04.png';
 import img05 from './gallery-images/img-05.png';
 // import img06 from './gallery-images/img-06.png';
-// import img07 from './gallery-images/img-07.png';
+import img07 from './gallery-images/img-07.png';
 // import img08 from './gallery-images/img-08.png';
 
 // export const gallery = [
@@ -49,4 +49,64 @@ export const projects = [
       'https://github.com/SvetlanaStoycheva/frontend-mentor-challenges-React-JS/tree/main/04-Project-Clock-app',
     borderClass: 'border02',
   },
+  {
+    id: 3,
+    title: 'IP Address Tracker',
+    subtitle: 'HTML/CSS/React JS/APIs',
+    info:
+      'Frontend Mentor challenge. This App uses an API to get the IP Address location and a react map library to generate the map. On the initial page load, the user can see his/her own IP Address on the map. They can search for any IP addresses or domains and see the key information and location.',
+    img: img05,
+    url: 'https://sweta-fm-ip-tracker.netlify.app',
+    sourceCode:
+      'https://github.com/SvetlanaStoycheva/frontend-mentor-challenges-React-JS/tree/main/03-Project-IP%20Address%20Tracker',
+    borderClass: 'border03',
+  },
+  {
+    id: 4,
+    title: 'Space Travel Company Website - Your Seat To Space',
+    subtitle: 'HTML/CSS/Vanilla JS/Netlify serverless functions',
+    info:
+      "The project is inspired by Comfy project from John Smilga's JavaScript tutorial. The displayed data is fetched from a Netlify serverless function and it is saved on the locale Storage. The user can choose and order his/her preferred space travel adventure from many, that are offered by different companies like SpaceX, Virgin Galactic, etc. The website contains: home page with featured flights and a slider, flights page with filtering options by company and price, single flight page with detailed information and shopping cart",
+    img: img01,
+    url: 'https://your-seat-to-space.netlify.app',
+    sourceCode:
+      'https://github.com/SvetlanaStoycheva/Vanilla-JS-online-store-travelling-company-Your-Seat-To-Space',
+    borderClass: 'border04',
+  },
+  {
+    id: 5,
+    title: 'URL shortening landing page',
+    subtitle: 'HTML/CSS/React JS/API',
+    info:
+      "Frontend Mentor challenge. The app is a landing page that contains a Link Shortener form, so that the user can make any link shorter and easier to remember. The short link gets copied to the user's clipboard in a single click. There is an error message when the form is submitted with empty input field or incorrect url. The user can see a list of their shortened links, even after refreshing the browser (local storage)",
+    img: img07,
+    url: 'https://sweta-fm-url-shortening.netlify.app',
+    sourceCode:
+      'https://github.com/SvetlanaStoycheva/frontend-mentor-challenges-React-JS/tree/main/01-Project-url-shortening-api',
+    borderClass: 'border05',
+  },
+  {
+    id: 6,
+    title: 'Website for ski quiding company PowderWise Aspen',
+    subtitle: 'CSS Flexbox',
+    info:
+      'The project is inspired by the CSS flexbox project in the John Smilga CSS tutorial. The app is a responsive website that contains: navbar/sidebar, hero with title and call-to-action button, about section, services section, featured tours (cards with hover effects), contact form, gallery and footer',
+    img: img04,
+    url: 'https://sweta-css-flexbox-ski-touring-powderwise.netlify.app',
+    sourceCode:
+      'https://github.com/SvetlanaStoycheva/CSS-flexbox-PowderWise-ski-touring-Aspen',
+    borderClass: 'border06',
+  },
+  // {
+  //   id: 7,
+  //   title: 'Clock app',
+  //   subtitle: 'HTML/CSS/React JS/APIs',
+  //   info:
+  //     "Frontend Mentor challenge. Using external APIs to set the clock data based on the visitor's location and to show random generated quotes. User can: View the current time and location information based on their IP address; View additional information about the date and time in the expanded state (sidebar); Be shown the correct greeting and background image based on the time of day they're visiting the site; Generate random programming quotes by clicking the refresh icon near the quote",
+  //   img: img03,
+  //   url: 'https://sweta-fm-clock-app.netlify.app',
+  //   sourceCode:
+  //     'https://github.com/SvetlanaStoycheva/frontend-mentor-challenges-React-JS/tree/main/04-Project-Clock-app',
+  //   borderClass: 'border07',
+  // },
 ];
